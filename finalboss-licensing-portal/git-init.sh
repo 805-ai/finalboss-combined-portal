7549516315f4ac90c34fd48c44b2a0314f775f1c
805-ai <abman21111@gmail.com>
2025-08-02 22:30:14 -0700

    Create git-init.sh

---
+++ finalboss-licensing-portal/git-init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git init
+git remote add origin https://github.com/805-ai/finalboss-licensing-portal.git
+git add .
+git commit -m "Initial Final Boss Portal Commit"
+git push -u origin master
